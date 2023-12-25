48ade7feec0b8521021e1d8117f53af3190bd7c0
Kirill Kabanov <kirillkk23@gmail.com>
2023-12-25 22:21:13 +0500

    3

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import asyncio
 import logging
+import re
 import sys
 from os import getenv
 import aiohttp
-
+from aiogram.types import InlineKeyboardMarkup
 
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
@@ -23,21 +24,36 @@ API_TOKEN = getenv("api_token")
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
-def expand_film(raw_data):
-    film_name = raw_data['docs'][0]['name']
-    film_desc = response.json()['docs'][0]['description']
-    film_short_desc = response.json()['docs'][0]['shortDescription']
-    rating_kp = response.json()['docs'][0]['rating']['kp']
-    rating_imdb = response.json()['docs'][0]['rating']['imdb']
-    url_poster = response.json()['docs'][0]['poster']['url']
+# def expand_film(raw_data):
+#     film_name = raw_data[0]['name']
+#     film_desc = response.json()[0]['description']
+#     film_short_desc = response.json()[0]['shortDescription']
+#     rating_kp = response.json()[0]['rating']['kp']
+#     rating_imdb = response.json()[0]['rating']['imdb']
+#     url_poster = response.json()[0]['poster']['url']
+
+
+def create_answer(film: dict) -> str:
+
+    inline_buttons = InlineKeyboardMarkup(row_width=1)
+    ans = f'{film["name"]}\n'\
+          f'{film["shortDescription"]}' \
+          f'{film["shortDescription"]}' \
+          f'Смотреть фильм тут: inline_buttons.add(InlineKeyboardButton("Смотреть на КинопоискHD", kinopoisk_hd_url))'
+
+
+    return ans
+
 
 async def get_film_kinopoisk(film_name: str):
     url = f'https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=10&query={film_name}'
     headers = {'accept': 'application/json', 'X-API-KEY': 'TJ60QMS-0PF465Y-H6TRPMH-TCKCB9W'}
     async with aiohttp.request('GET', url, headers=headers) as response:
         kp_resp = await response.json()
-        raw_film_data = kp_resp["docs"][0]
-        return expand_film(raw_film_data)
+        film = kp_resp["docs"][0]
+        if prepare_film_name(film['name'])== film_name or prepare_film_name(film['alternativeName']) == film_name:
+            return film
+
 
 # loop = asyncio.get_event_loop()
 # response = loop.run_until_complete(make_request())
@@ -54,6 +70,9 @@ async def command_start_handler(message: Message) -> None:
                          'Я бот, который выдает ссылки на просмотр фильмов.'
                          'Какой фильм ты хочешь посмотреть?')
 
+def prepare_film_name(name):
+    return re.sub(r'[^\w\s]', '', name.lower()).replace('ё', 'е')
+
 
 @dp.message()
 async def get_film(message: types.Message) -> None:
@@ -63,12 +82,14 @@ async def get_film(message: types.Message) -> None:
     By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
     #try:
-    text = message.text
-    print(text)
-    if not text:
-        await message.answer('Иди нахуй, долбоёб!!!')
+    film = prepare_film_name(message.text)
+    if not film:
+        await message.answer('Не нашли такой фильм.')
     else:
-        await message.send_copy(chat_id=message.chat.id)
+        film = await get_film_kinopoisk(film)
+        await message.answer(create_answer(film))
+
+
     # except TypeError:
     #     # But not all the types is supported to be copied so need to handle it
     #     await message.answer("Nice try!")
